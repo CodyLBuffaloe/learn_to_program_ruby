90c4ccdac77b77030609a0365b1b3a8f9d4b58c0
Cody Buffaloe <codylbuffaloe@gmail.com>
2016-01-14 17:26:47 -0500

    Fixed infinite loop, finished program

--- learn_to_program_challenge5.rb
+++ learn_to_program_challenge5.rb
@@ -1,7 +1,8 @@
 puts "Type a word and hit 'Enter'. Do this as many times as you like. When you're done, just hit 'Enter' a second time. "
-word = gets
+word = gets.chomp
 list = []
-while (word != " ")
+while(word != "")
        list.push(word)
+       word = gets.chomp
 end
 puts list.sort
