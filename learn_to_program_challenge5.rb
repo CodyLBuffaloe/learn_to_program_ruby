6ffd77134a1e3455961d7569d1ad0f1a45253cdf
Cody Buffaloe <codylbuffaloe@gmail.com>
2016-01-14 17:38:08 -0500

    Working on the same problem, although without using .sort

--- learn_to_program_challenge5.rb
+++ learn_to_program_challenge5.rb
@@ -1,8 +1,27 @@
+#Takes an unlimited amount of strings, sorts them, repeats them back to you when you break the loop
+
 puts "Type a word and hit 'Enter'. Do this as many times as you like. When you're done, just hit 'Enter' a second time. "
+
 word = gets.chomp
+
 list = []
+
 while(word != "")
+
        list.push(word)
+
        word = gets.chomp
 end
+
 puts list.sort
+
+#The same program as above, without using .sort
+
+puts "Type a word, then hit 'Enter'. Do this as many times as you want. To quit, just hit 'Enter' a second time."
+
+word = gets.chomp
+
+list = []
+
+while(word != "")
+end
