330724705cdd4ef3b91cdab22755ec99bb82e1d1
Cody Buffaloe <codylbuffaloe@gmail.com>
2016-01-15 17:15:09 -0500

    Working on the second challenge, sorting without .sort

--- learn_to_program_challenge5.rb
+++ learn_to_program_challenge5.rb
@@ -1,3 +1,4 @@
+=begin
 #Takes an unlimited amount of strings, sorts them, repeats them back to you when you break the loop
 
 puts "Type a word and hit 'Enter'. Do this as many times as you like. When you're done, just hit 'Enter' a second time. "
@@ -14,14 +15,30 @@ while(word != "")
 end
 
 puts list.sort
-
+=end
 #The same program as above, without using .sort
 
 puts "Type a word, then hit 'Enter'. Do this as many times as you want. To quit, just hit 'Enter' a second time."
 
-word = gets.chomp
+word2 = gets.chomp
 
-list = []
+list2 = []
 
-while(word != "")
+sorted_list = []
+
+while(word2 != "")
+
+    list2.push(word2.downcase)
+
+    word2 = gets.chomp
 end
+list2.each{
+    |i|
+    a = 
+if(list2[i] > list2[a])
+    sorted_list.push(list2[0])
+else
+    sorted_list.push(list2[1])
+end
+}
+puts sorted_list
