54f9e8618a673f4baf135e3d2406a842d2b8a50c
Cody Buffaloe <codylbuffaloe@gmail.com>
2016-01-21 12:29:23 -0500

    Finished Class OrangeTree project. Tree ages, grows in height, produces varied amounts of oranges, you can count the oranges or pick them, and the tree eventually dies.

--- orange_tree.rb
+++ orange_tree.rb
@@ -1,61 +1,108 @@
 class OrangeTree
     def initialize
 
-        @age = 0
+        @age = 1
         @height = 1
         @dead = false
         @oranges = 0
+        puts "You planted an orange tree!"
 
     end
 
     def height
 
-        while(@dead != true)
-
-            if(@age > 1)
-
-                @height = @height + 3
-
-            end
-
-        end
+        puts "Your tree is #{@height} feet tall."
 
     end
 
     def oneYearPasses
-
-        if(@oranges == 0)
-            @year = @year + 1
-        end
+       @age = @age + 1
+       @height = @height + 2
+       @oranges = 0
+       if(@age >9)
+        deadTree
+       end
 
     end
 
-    def count_the_oranges
-        while(@dead != true)
-            if(@age > 3)
-                @orange_count = @orange_count + 5
-            else
-                puts "This tree is too young to produce oranges yet!"
-            end
-        end
-    end
+    def countTheOranges
 
-    def pick_an_orange
-        if(@oranges > 0)
-            @oranges = oranges -1
-            puts "You picked and orange and ate it. It was delicious!"
+        if((@age >1) and (@age < 5))
+            oneYearPasses
+            @oranges = 3
+            puts "Your tree is #{@age}! You have #{@oranges} oranges this year!"
+        elsif((@age > 4 ) and (@age < 7))
+            oneYearPasses
+            @oranges = 5
+
+            puts "Your tree is #{@age}! You have #{@oranges} oranges this year!"
+        elsif((@age > 6) and (@age < 9))
+            oneYearPasses
+            @oranges = 10
+
+            puts "Your tree is #{@age}! You have #{@oranges} oranges this year!"
+        elsif(@age < 2)
+            oneYearPasses
+            puts "Your tree is too young to produce oranges!"
         else
-            puts "You have no more oranges left!"
+            deadTree
         end
+        if(@oranges == 0)
+            oneYearPasses
+        end
+
+
     end
 
-    def tree_dead
-        if(@one_year_passes > 30)
-            @dead = true
+    def pickAnOrange
+
+        if(@oranges > 0)
+            @oranges = @oranges - 1
+            puts "You picked an orange. You have #{@oranges} oranges left."
+        else
+            puts "You have no oranges left to pick this year."
+            countTheOranges
+
+        end
+
+    end
+
+    def deadTree
+
+        if(@age > 9)
+
             puts "Your tree has died! :("
             exit
+        else
+            puts "Your tree is still alive! Hooray!"
+            oneYearPasses
         end
+
     end
+
 end
 
-puts OrangeTree.tree_dead
+tweedles = OrangeTree.new
+
+
+
+puts tweedles.countTheOranges
+puts tweedles.countTheOranges
+puts tweedles.countTheOranges
+
+puts tweedles.pickAnOrange
+puts tweedles.pickAnOrange
+puts tweedles.pickAnOrange
+puts tweedles.pickAnOrange
+puts tweedles.pickAnOrange
+puts tweedles.pickAnOrange
+puts tweedles.pickAnOrange
+
+puts tweedles.countTheOranges
+puts tweedles.countTheOranges
+puts tweedles.countTheOranges
+puts tweedles.countTheOranges
+
+
+
+
