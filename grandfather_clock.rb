2b02b419a14918c1659b76cfa5e014b1d724da06
Cody Buffaloe <codylbuffaloe@gmail.com>
2016-01-22 16:43:58 -0500

    Still working on grandfather clock, unsure of logic placement. Will try for full completion by tonight, if not, Monday.

--- grandfather_clock.rb
+++ grandfather_clock.rb
@@ -1,1 +1,21 @@
 #Takes a block of code, calls it once for each hour that passed, puts "Dong!" for each hour.
+
+time = Time.now.hour
+
+def grandfather_clock time, &block
+
+    if(time > 12)
+        time = time - 12
+        block.call
+    else
+        block.call
+    end
+
+end
+
+ grandfather_clock Time.now.hour do
+    time.times do
+    puts "Dong!"
+    end
+ end
+
